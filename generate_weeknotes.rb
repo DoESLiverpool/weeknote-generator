57286c5021af4e2e4d296947f1ca2ec441141c17
Patrick Fenner <patrick@defproc.co.uk>
2015-05-14 14:58:38 +0100

    added Community Content instructions at the end

--- generate_weeknotes.rb
+++ generate_weeknotes.rb
@@ -258,6 +258,10 @@ else
   content = content + "\n<p>Thanks " + issue_closers.uniq.collect { |c| "<a href='"+c["html_url"]+"'>"+c["login"]+"</a>" }.join(", ") + "!</p>"
 end
 
+# Add a note on how to send blog posts for publishing
+content = content + "\n<h3>Community Content</h3>"
+content = content + "\n<p>DoES exists to support the community that uses DoES. If you use DoES, then you are part of that community. If you would like to publisise something related to DoES, you can email <a href=\"mailto:hello@doesliverpool.com\">hello@doesliverpool.com</a> with the formated content for us to use as a new blog post. Tell us what you're up to, and we'll show the world what epic stuff happens at DoES!</p>"
+
 
 # Post it up as a draft post
 post = {
