43e0e1f9f8dfb91ca7d735998445fbead7553705
Adrian McEwen <adrianm@mcqn.com>
2016-02-01 15:07:42 +0000

    Exclude retweets from results.  Fixes #15

--- generate_weeknotes.rb
+++ generate_weeknotes.rb
@@ -96,7 +96,7 @@ unless input_settings["twitter"].nil?
   end
   settings["tags"].each do |tag|
     twitter_client.search("##{tag}", :count => 100, :result_type => "recent").each do |tweet|
-      if tweet.created_at >= start_of_last_week && tweet.created_at <= end_of_last_week
+      if tweet.created_at >= start_of_last_week && tweet.created_at <= end_of_last_week && !tweet.retweet?
         if tweet.user.following? || tweet.user.id == input_settings["twitter"]["id"]
           # It's a tweet in the past week containing "#weeknotes",
           # from someone we're following (or from us!)
